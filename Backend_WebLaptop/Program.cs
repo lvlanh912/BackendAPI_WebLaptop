fab295c8943ff2374e510684d8605430942a0456
Lv Lanh <lvlanh912@gmail.com>
2024-05-01 00:16:05 +0700

    next

--- Backend_WebLaptop/Program.cs
+++ Backend_WebLaptop/Program.cs
@@ -39,7 +39,7 @@ builder.Services.Configure<VNPayconfig>(
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("MyCorsPolicy",
-        builder => builder.WithOrigins("http://localhost:5173")
+        builder => builder.WithOrigins("http://localhost:5173","https://5173-cs-3339b1ce-3180-410e-90e7-cb3e72a03e34.cs-asia-east1-vger.cloudshell.dev")
                            .AllowAnyMethod()
                            .AllowAnyHeader()
                             .AllowAnyOrigin()
